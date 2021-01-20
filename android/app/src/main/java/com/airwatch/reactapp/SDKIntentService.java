2790b4947b702542f29b97a93291a1b81f807b28
Amit Kalghatgi <akalghatgi@vmware.com>
2021-01-20 20:20:22 +0530

    release-1.0 app

---
+++ android/app/src/main/java/com/airwatch/reactapp/SDKIntentService.java
@@ -0,0 +1,40 @@
+package com.airwatch.reactapp;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.airwatch.sdk.profile.AnchorAppStatus;
+import com.airwatch.sdk.profile.ApplicationProfile;
+import com.airwatch.sdk.shareddevice.ClearReasonCode;
+import com.workspaceonesdk.WorkspaceOneSDKIntentService;
+
+public class SDKIntentService extends WorkspaceOneSDKIntentService {
+    @Override
+    protected void onApplicationConfigurationChange(Bundle bundle) {
+
+    }
+
+    @Override
+    protected void onApplicationProfileReceived(Context context, String s, ApplicationProfile applicationProfile) {
+        Log.d("SDK Init","onApplicationProfileReceived");
+    }
+
+    @Override
+    protected void onClearAppDataCommandReceived(Context context, ClearReasonCode clearReasonCode) {
+        Log.d("SDK Init","onClearAppDataCommandReceived");
+
+    }
+
+    @Override
+    protected void onAnchorAppStatusReceived(Context context, AnchorAppStatus anchorAppStatus) {
+
+    }
+
+    @Override
+    protected void onAnchorAppUpgrade(Context context, boolean b) {
+
+    }
+
+}
---
+++ components/List.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { FlatList, StyleSheet, Text, TouchableOpacity } from 'react-native'
+
+export default function List({ items, onPressItem }) {
+  return (
+    <FlatList
+      data={items}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item, index }) => (
+        <TouchableOpacity
+          style={[styles.item, { backgroundColor: itemColor(index) }]}
+          onPress={() => onPressItem(item)}
+        >
+        <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.desc}>{item.desc}</Text>
+        </TouchableOpacity>
+      )}
+    />
+  )
+}
+
+function itemColor(index) {
+  return `rgba(59, 108, 212, ${Math.max(1 - index / 10, 0.4)})`
+}
+
+const styles = StyleSheet.create({
+  item: {
+    marginBottom: 1,
+    padding: 15,
+  },
+  title: {
+    fontSize: 24,
+    color: 'white',
+  },
+  desc: {
+    fontSize: 16,
+    color: 'white',
+  },
+})
